95ec8649e20cfb7e8ae5f63e88ff1da7eecd044e
JosR <josueramos17@hotmail.com>
2023-10-11 12:04:56 -0500

    feat: add new-credit component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent} from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
-import { NotFoundComponent} from "./views/not-found/not-found.component";
+import { NotFoundComponent} from "./shared/not-found/not-found.component";
 import {HomeComponent} from "./views/home/home.component";
+import {NewCreditComponent} from "./views/new-credit/new-credit.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'new-credit', component: NewCreditComponent },
   { path: '',   redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
