72a3370116cb1b5436d835d40994c3504d1583e5
JosR <josueramos17@hotmail.com>
2023-11-13 10:53:13 -0500

    chore: add missing column

--- src/app/views/credits/credits.component.ts
+++ src/app/views/credits/credits.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./credits.component.scss']
 })
 export class CreditsComponent {
-  displayedColumns: string[] = ['fee-number', 'final-fee-initial-balance',
+  displayedColumns: string[] = ['fee-number', 'grace-period', 'final-fee-initial-balance',
     'final-fee-interest', 'final-fee-amort', 'final-fee-life-insurance',
     'final-fee-final-balance', 'interest', 'fee', 'amortization', 'fee-life-insurance',
     'risk-insurance', 'gps', 'ports', 'admin-bills', 'fee-final-balance', 'flow'];
